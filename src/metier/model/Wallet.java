fd9ef295e3327e9c948a6cb2be7d7d957615ec5f
Kawtar <Kawtar.shaimi8@gmail.com>
2025-09-29 19:37:56 +0100

    CYP-7 : Création du classe Wallet

---
+++ src/metier/model/Wallet.java
@@ -0,0 +1,26 @@
+package metier.model;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+public abstract class Wallet {
+
+	private final String id;
+	private final String address;
+	private BigDecimal balance;
+	private final Instant createdAt;
+
+	protected Wallet( String address) {
+		this.id = UUID.randomUUID().toString();		this.address = address;
+		this.balance = BigDecimal.ZERO;
+		this.createdAt = Instant.now();
+	}
+
+	public String getId() { return id; }
+	public String getAddress() { return address; }
+	public BigDecimal getBalance() { return balance; }
+	public Instant getCreatedAt() { return createdAt; }
+	public void setBalance(BigDecimal balance) { this.balance = balance; }
+
+}
