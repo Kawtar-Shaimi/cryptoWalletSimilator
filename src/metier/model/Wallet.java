223fb8d6e5e2e2edef4fc07199d3ef362f0c07eb
Kawtar <Kawtar.shaimi8@gmail.com>
2025-10-01 11:51:23 +0100

    CYP-7 : Terminer la classe de Wallet.

--- src/metier/model/Wallet.java
+++ src/metier/model/Wallet.java
@@ -2,26 +2,40 @@ package metier.model;
 
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
+import metier.enums.CryptoType;
 
 public abstract class Wallet {
 
 	private final String id;
+	private final CryptoType cryptoType;
 	private final String address;
 	private BigDecimal balance;
 	private final Instant createdAt;
+    private final List<metier.model.Transaction> transactions;
 
-  protected Wallet(String address) {
+	protected Wallet(CryptoType cryptoType, String address) {
 		this.id = UUID.randomUUID().toString();
+		this.cryptoType = cryptoType;
 		this.address = address;
 		this.balance = BigDecimal.ZERO;
 		this.createdAt = Instant.now();
+        this.transactions = new ArrayList<metier.model.Transaction>();
 	}
-  
+
 	public String getId() { return id; }
+	public CryptoType getCryptoType() { return cryptoType; }
 	public String getAddress() { return address; }
 	public BigDecimal getBalance() { return balance; }
 	public Instant getCreatedAt() { return createdAt; }
+    public List<metier.model.Transaction> getTransactions() { return transactions; }
+
 	public void setBalance(BigDecimal balance) { this.balance = balance; }
+
+    public void addTransaction(metier.model.Transaction tx) { this.transactions.add(tx); }
 }
+
+
